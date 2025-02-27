5859bec1d4336da70be847dd0a1440e28136585e
Julian Maynes <thelastpocket@gmail.com>
2025-02-27 00:50:04 -0800

    Create repo

---
+++ main.py
@@ -0,0 +1,65 @@
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+from PySide6.QtGui import QPixmap, QMovie
+from PySide6.QtCore import Qt, QRect, QByteArray, QBuffer
+import sys
+import requests
+import urllib.request
+
+import asyncio
+import seventv
+
+class GifWindow(QMainWindow):
+
+    def __init__(self, type , emote):
+        super().__init__()
+        self.emote_url = None
+        self.SearchEmote(self.emote_url)
+        # Resize main window to be 600px / 400px
+        self.resize(600, 400)
+        self.MovieLabel = QLabel(self)
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.SearchEmote(emote))
+        
+
+    def load_gif(self):
+
+        response = requests.get(self.emote_url)
+        if response.status_code == 200:
+            gif_data = response.content  # Get GIF bytes
+
+            # Store GIF bytes in QBuffer (acts like a file)
+            self.buffer = QBuffer()
+            self.buffer.setData(QByteArray(gif_data))
+            self.buffer.open(QBuffer.ReadOnly)
+
+        # Set gif content to be same size as window (600px / 400px)
+        self.MovieLabel.setGeometry(QRect(0, 0, 600, 400))
+        self.MovieLabel.setAlignment(Qt.AlignCenter)
+        self.movie = QMovie(self.buffer)
+        self.MovieLabel.setMovie(self.movie)
+        self.movie.start()
+        
+
+    def change_emote(self, new_gif):
+        self.emote_url = self.SearchEmote(new_gif)
+
+    async def SearchEmote(self, emote : str) -> str:
+        async with seventv.seventv() as mySevenTvSession:
+            emotes = await mySevenTvSession.emote_search(emote, case_sensitive=True)
+
+        if emotes:
+            self.emote_url = "https:" + emotes[0].host_url + "/4x.gif"
+            self.load_gif
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GifWindow("name", "AINTNOWAY")
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    # window = GifWindow("https://cdn.7tv.app/emote/01JFEY3QWV7EW547PAVX19ZWNF/4x.webp")
+    window.show()
+    loop.run_until_complete(asyncio.sleep(0))
+    sys.exit(app.exec_())
+
